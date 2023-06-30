9a3a86d83f96f3087705ed970a1cd5284c0b8f0d
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-30 22:39:21 +0530

    [Shantanu]: get number of contact persons by type code is added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -18,4 +18,10 @@ select city, state, count(*) as size from AddressBook group by city, state;
 
 select * from AddressBook where city = 'maha' order by firstname, lastname;
 
+alter table AddressBook add name varchar(20), type varchar(20);
+update AddressBook set type='my address book' where firstname = 'ram' and lastname = 'shinde';
+update AddressBook set type='my address book' where firstname = 'raj' and lastname = 'shinde';
+update AddressBook set type='my address book' where firstname = 'shyam' and lastname = 'shinde';
+update AddressBook set type='vip persons' where firstname = 'rahul' and lastname = 'shinde';
+
 
