bbf98401b53f01efafe567f19b77b87877108d67
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-30 20:22:53 +0530

    [Shantanu]: insert data code is added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,3 +3,5 @@ create database AddressBookService
 create table AddressBook(firstname varchar(50), lastname varchar(50), address varchar(50), city varchar(50), state varchar(50), 
 zip int, phonenumber bigint, email varchar(50));
 
+insert into AddressBook values('ram','shinde','xyz','maha','maharashtra',401104,421571525,'ramshinde@gmail.com');
+select * from AddressBook;
