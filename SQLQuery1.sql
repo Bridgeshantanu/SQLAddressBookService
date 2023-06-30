4b34acf8451d981a8a78e8a090fc914f4d6754eb
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-30 20:55:05 +0530

    [Shantanu]: understand size by city or state code is added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,8 +3,8 @@ create database AddressBookService
 create table AddressBook(firstname varchar(50), lastname varchar(50), address varchar(50), city varchar(50), state varchar(50), 
 zip int, phonenumber bigint, email varchar(50));
 
-insert into AddressBook values('ram','shinde','xyz','maha','maharashtra',401104,421571525,'ramshinde@gmail.com'),
-('raj','shinde','xyz','maha','maharashtra',401104,421571525,'rajshinde@gmail.com');
+insert into AddressBook values('shyam','shinde','xyz','maha','maharashtra',401104,421571525,'ramshinde@gmail.com'),
+('rahul','shinde','xyz','maha','maharashtra',401104,421571525,'rajshinde@gmail.com');
 select * from AddressBook;
 
 update AddressBook set address='abc', city='def', state='erer', zip=554455, phonenumber=78522622,
@@ -13,5 +13,8 @@ email='ramshinde45@gmail.com' where firstname = 'ram' and lastname = 'shinde';
 delete from AddressBook where firstname = 'raj' AND lastname = 'shinde';
 
 select * from AddressBook where city = 'maha' OR state = 'maharashtra';
+
+select city, state, count(*) as size from AddressBook group by city, state;
+
 
 
