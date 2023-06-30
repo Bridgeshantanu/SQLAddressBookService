d0b788e7649f3321a3b2dfb49a11e062bfe2203b
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-30 20:14:01 +0530

    [Shantanu]: create table code is added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,5 @@
 create database AddressBookService
+
+create table AddressBook(firstname varchar(50), lastname varchar(50), address varchar(50), city varchar(50), state varchar(50), 
+zip int, phonenumber bigint, email varchar(50));
+
